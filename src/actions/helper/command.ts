16c3e8b0273cc2e1973cf52d6fafa16edfbd87a7
batuhan <batuhancaglayan1@gmail.com>
2021-10-14 13:39:13 +0300

    add command helper

---
+++ src/actions/helper/command.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import Minimist from 'minimist'
+
+import ShellQuote from 'shell-quote'
+
+// import { getScript } from './package'
+
+export function parseCommand(command: string): any[] {
+    return ShellQuote.parse(command)
+}
+
+export function minimistArgs(args: string[]): any {
+    return Minimist(args)
+}
+
+export function getCommandPart(args: string[]): any {
+    return minimistArgs(args)['_']
+}
